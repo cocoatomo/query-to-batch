e090b921db1a7fbe848f841f10908089a697bb88
cocoatomo <cocoatomo77@gmail.com>
2013-08-25 21:46:52 +0900

    define CREATE TABLE statement

--- src/main/java/net/elliptium/qb/App.java
+++ src/main/java/net/elliptium/qb/App.java
@@ -4,10 +4,10 @@ package net.elliptium.qb;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        // PlSql parser = new PlSql();
     }
+
 }
---
+++ src/main/sql/sample.sql
@@ -0,0 +1,2 @@
+-- select * from A left outer join B on A.id = B.id;
+create table hoge ( id number );
